64ab70d3acb0884dd85c1821c30604af998205f9
Md. Abdur Rahim Sarkar <rahimbd623@gmail.com>
2020-05-24 19:47:37 +0600

    include Admin Deshboard

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -37,7 +37,7 @@ export default function Components(props) {
         }}
         {...rest}
       />
-      <Parallax image={require("assets/img/bg7.jpg")}>
+      <Parallax image={require("assets/img/cov.png")}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem>
